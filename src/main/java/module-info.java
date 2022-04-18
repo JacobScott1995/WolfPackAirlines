a489e49987cb8f5441edec9b5e97e466e62479b9
Jacob <jrscot11@gmail.com>
2022-04-18 13:51:49 -0700

    Init Upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.wolfpackairlines {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.wolfpackairlines to javafx.fxml;
+    exports com.example.wolfpackairlines;
+}
