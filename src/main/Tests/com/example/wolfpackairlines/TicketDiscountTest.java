16bed4929edb5aa86f55918d650f926861e64441
rayalva407 <rayalva407@gmail.com>
2022-04-19 10:27:26 -0600

    TicketDiscount method and tests

---
+++ src/main/Tests/com/example/wolfpackairlines/TicketDiscountTest.java
@@ -0,0 +1,33 @@
+package com.example.wolfpackairlines;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketDiscountTest {
+
+    TicketDiscount test1;
+
+    @BeforeEach
+    void setup() {
+        test1 = new TicketDiscount();
+    }
+
+    @Test
+    void ticketDiscount() {
+        double newPrice = test1.ticketDiscount(10,"Male", 100.00);
+        assertEquals(50.00, newPrice);
+
+        newPrice = test1.ticketDiscount(80,"Male", 238.50);
+        assertEquals(95.40, newPrice);
+
+        newPrice = test1.ticketDiscount(18,"Female", 250.50);
+        assertEquals(187.87, newPrice);
+
+        newPrice = test1.ticketDiscount(60,"Female", 185.75);
+        assertEquals(55.72, newPrice);
+
+    }
+}
---
+++ src/main/java/com/example/wolfpackairlines/TicketDiscount.java
@@ -0,0 +1,30 @@
+package com.example.wolfpackairlines;
+
+public class TicketDiscount {
+
+    public double ticketDiscount(int age, String gender, double originalPrice) {
+        double newPrice = 0;
+        if (age <= 12) {
+            newPrice = originalPrice - (originalPrice * .50);
+            if (gender.equals("Female")) {
+                newPrice = newPrice - (newPrice * .25);
+            }
+        }
+        else if (age >= 60) {
+            newPrice = originalPrice - (originalPrice * .60);
+            if (gender.equals("Female")) {
+                newPrice = newPrice - (newPrice * .25);
+            }
+        }
+        else if (gender.equals("Female")) {
+            newPrice = originalPrice - (originalPrice * .25);
+        }
+
+        //truncates to two decimal values
+        newPrice = newPrice * 100;
+        newPrice = Math.floor(newPrice);
+        newPrice = newPrice / 100;
+
+        return newPrice;
+    }
+}
