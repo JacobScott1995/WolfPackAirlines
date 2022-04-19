34ce9f684f690924f5acd672fc785284cb14c23a
Johnathan Regan <hyperspaceworks@gmail.com>
2022-04-19 15:30:29 -0400

    added csv and mehtod
    added flight csv, boarding pass csv and collectdata method

---
+++ src/main/java/com/example/wolfpackairlines/File_Writer.java
@@ -0,0 +1,33 @@
+package com.example.wolfpackairlines;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class File_Writer {
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
+    public static void addCustomerData(ArrayList<String> arrstr) throws IOException {
+        File file = new File("src/main/resources/Boarding_Pass_Data.csv");
+
+        FileWriter outputfile = new FileWriter(file, true);
+        CSVWriter writer = new CSVWriter(outputfile);
+
+
+        writer.writeAll((Iterable<String[]>) arrstr);
+
+
+        writer.close();
+    }
+
+
+}
